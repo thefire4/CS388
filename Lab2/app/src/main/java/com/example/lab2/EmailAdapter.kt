3342ff1eac1fec4ca7510e783441e2f75d7ea8ab
Carlos <clc52@njit.edu>
2026-02-06 16:34:51 -0500

    Adding unfinished Lab2

---
+++ Lab2/app/src/main/java/com/example/lab2/EmailAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.lab2
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class EmailAdapter {
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        // Your holder should contain a member variable for any view that will be set as you render
+        // a row
+        val senderTextView: TextView
+        val titleTextView: TextView
+        val summaryTextView: TextView
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each sub-view
+        init {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            senderTextView = itemView.findViewById(R.id.senderTv)
+            titleTextView = itemView.findViewById(R.id.titleTv)
+            summaryTextView = itemView.findViewById(R.id.summaryTv)
+        }
+    }
+}
