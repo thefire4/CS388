a9e68a8b326cdb3016aa974ade1954ec18c125ba
thefire4 <carloscabanilla09@gmail.com>
2026-02-06 21:30:44 -0500

    Finishing Lab 2

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.lab2
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var emails: MutableList<Email>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,25 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val emailsRv = findViewById<RecyclerView>(R.id.emailsRv)
+        // Fetch the list of emails
+        emails = EmailFetcher.getEmails()
+        // Create adapter passing in the list of emails
+        val adapter = EmailAdapter(emails)
+        // Attach the adapter to the RecyclerView to populate items
+        emailsRv.adapter = adapter
+        // Set layout manager to position the items
+        emailsRv.layoutManager = LinearLayoutManager(this)
+
+        findViewById<Button>(R.id.loadMoreBtn).setOnClickListener {
+            // Fetch next 5 emails
+            val newEmails = EmailFetcher.getNext5Emails()
+            // Add new emails to existing list of emails
+            emails.addAll(newEmails)
+            // Notify the adapter there's new emails so the RecyclerView layout is updated
+            adapter.notifyDataSetChanged()
         }
     }
 }
