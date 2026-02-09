9ec5e068bda2710b2e91413adb037fcad7178f34
Carlos <clc52@njit.edu>
2026-02-09 17:49:47 -0500

    Finished Project2

--- Project2/app/src/main/java/com/example/project2/MainActivity.kt
+++ Project2/app/src/main/java/com/example/project2/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var itemURL: EditText
     lateinit var itemPrice: EditText
     lateinit var submitBtn : Button
+    lateinit var listItems: MutableList<WishList>
+    lateinit var adapter: WishlistAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -22,22 +26,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            val ime = insets.getInsets(WindowInsetsCompat.Type.ime())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, maxOf(systemBars.bottom, ime.bottom))
             insets
         }
 
+        val wishlistRv = findViewById<RecyclerView>(R.id.wishRV)
+
+        listItems = mutableListOf()
+        adapter = WishlistAdapter(listItems) {position ->
+                listItems.removeAt(position)
+                adapter.notifyItemRemoved(position)
+        }
         itemName = findViewById<EditText>(R.id.itemName)
         itemURL = findViewById<EditText>(R.id.itemURL)
         itemPrice = findViewById<EditText>(R.id.itemPrice)
         submitBtn = findViewById<Button>(R.id.submitBtn)
 
-
+        wishlistRv.adapter = adapter
+        wishlistRv.layoutManager = LinearLayoutManager(this)
 
         submitBtn.setOnClickListener {
             var name = itemName.text.toString().trim()
-            var price = itemPrice.text
-            var url = itemPrice.text.toString().trim()
+            var price = itemPrice.text.toString().toFloat()
+            var url = itemURL.text.toString().trim()
+            val item = WishList(name, price, url)
+            listItems.add(item)
 
+            itemName.text.clear()
+            itemPrice.text.clear()
+            itemURL.text.clear()
         }
+
+
     }
 }
--- Project2/app/src/main/java/com/example/project2/WishlistAdapter.kt
+++ Project2/app/src/main/java/com/example/project2/WishlistAdapter.kt
@@ -1,28 +1,57 @@
 package com.example.project2;
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class WishlistAdapter {
+class WishlistAdapter(private val listItems: MutableList<WishList>, private val longPress: (Int) -> Unit) : RecyclerView.Adapter<WishlistAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(R.layout.wishlist_item, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Get the data model based on position
+        val items = listItems.get(position)
+        // Set item views based on views and data model
+        holder.itemTextView.text = items.itmName
+        holder.priceTextView.text = String.format("$%.2f", items.itmPrice)
+        holder.urlTextView.text = items.itmURL
+
+        holder.itemTextView.setOnLongClickListener {
+            longPress(holder.bindingAdapterPosition)
+            true
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listItems.size
+    }
+
     // Provide a direct reference to each of the views within a data item
     // Used to cache the views within the item layout for fast access
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         // Your holder should contain a member variable for any view that will be set as you render
         // a row
-        val senderTextView: TextView
-        val titleTextView: TextView
-        val summaryTextView: TextView
+        val itemTextView: TextView
+        val priceTextView: TextView
+        val urlTextView: TextView
 
         // We also create a constructor that accepts the entire item row
         // and does the view lookups to find each sub-view
         init {
             // Stores the itemView in a public final member variable that can be used
             // to access the context from any ViewHolder instance.
-            senderTextView = itemView.findViewById(R.id.itemTV)
-            titleTextView = itemView.findViewById(R.id.priceTV)
-            summaryTextView = itemView.findViewById(R.id.urlTV)
+            itemTextView = itemView.findViewById(R.id.itemTV)
+            priceTextView = itemView.findViewById(R.id.priceTV)
+            urlTextView = itemView.findViewById(R.id.urlTV)
         }
     }
 }
